5560cfdb9ab80f6d7744c439ddc37ebd970ca37b
Padmashree Nuggehalli Srinivasa <pa1565nu-s@krok.fransg.eit.lth.se>
2022-03-31 17:03:58 +0200

    TODO

--- scripts/power.tcl
+++ scripts/power.tcl
@@ -9,20 +9,17 @@ set power_enable_analysis TRUE
 set power_analysis_mode time_based
 
 # Link and setup design libraries
-# TODO: Make the list where lib dirs are exported? Synth and layout share some of thoese
+# TODO: Make the list where lib dirs are exported? Synth and layout share some of thoese, check synth_prefs.tcl
 set search_path "$::env(STM065_DIR)/$::env(CLK_LIB)/libs $::env(STM065_DIR)/$::env(BUF_LIB)/libs $::env(STM065_DIR)/IO65LPHVT_SF_1V8_50A_7M4X0Y2Z_7.0/libs $::env(STM065_DIR)/IO65LP_SF_BASIC_50A_ST_7M4X0Y2Z_7.2/libs $::env(PADS_LIB) $::env(RAM_LIB)/libs $search_path"
 
 # TODO: This is not flexible, fix this, same names are used in synthesis
 set link_path "* \
-SPHD110420_wc_1.10V_125C.db \
-CLOCK65LPHVT_nom_1.30V_125C.db \
-CORE65LPHVT_nom_1.30V_125C.db \
-Pads_Oct2012.db \
+	SPHD110420_wc_1.10V_125C.db \
+	CLOCK65LPHVT_nom_1.30V_125C.db \
+	CORE65LPHVT_nom_1.30V_125C.db \
+	Pads_Oct2012.db \
 	IO65LPHVT_SF_1V8_50A_7M4X0Y2Z_wc_0.90V_1.65V_125C.db \
 	IO65LP_SF_BASIC_50A_ST_7M4X0Y2Z_wc_0.90V_125C.db"
-
-# set link_library    "$library_additions $link_library"
-# set target_library    "$library_additions $target_library"
 
 # Design input
 read_verilog $::env(TOP_FILE_L)
